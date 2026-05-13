8e20e449d0da6f79493d42ef4c1cd4a0e2d238cb
Nic Cheneweth <nchenewe@thoughtworks.com>
2026-05-12 23:04:36 -0500

    [nc] change location for providerconfig
    Signed-off-by: Nic Cheneweth <nchenewe@thoughtworks.com>

--- scripts/generate_application_resource.sh
+++ scripts/generate_application_resource.sh
@@ -68,7 +68,7 @@ spec:
   source:
     repoURL: https://github.com/twplatformlabs/psk-aws-control-plane-configuration
     targetRevision: HEAD
-    path: roles/$cluster_role/crossplane/providerconfig.yaml
+    path: roles/$cluster_role/crossplane/providerconfig
   destination:
     server: https://kubernetes.default.svc
     namespace: crossplane-system
@@ -88,4 +88,5 @@ EOF
 
 cp deploy-templates/default-values.yaml deploy-files/default-values.yaml
 cp deploy-templates/$cluster_role-values.yaml deploy-files/$cluster_role-values.yaml
-cp deploy-templates/providerconfig.yaml deploy-files/providerconfig.yaml
+mkdir deploy-files/providerconfig
+cp deploy-templates/providerconfig.yaml deploy-files/providerconfig/providerconfig.yaml
